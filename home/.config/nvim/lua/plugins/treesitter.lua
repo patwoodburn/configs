d99ebc9dd30d4d9180ca4cc13adcb8709921d015
Patrick Woodburn <pat@patrickwoodburn.com>
2026-03-04 21:08:42 -0500

    thing

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,11 @@
 local plugin = {	"nvim-treesitter/nvim-treesitter" }
 plugin.build = ":TSUpdate"
-function plugin.config()
-  local configs = require("nvim-treesitter.configs")
-	configs.setup({
-		ensure_installed = { "lua", },
-    ignore_install = {"org"},
-		auto_install = true,
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
-	})
-end
+plugin.opts = {
+  ensure_installed = { "lua", },
+  ignore_install = {"org"},
+  auto_install = true,
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+}
 return plugin
